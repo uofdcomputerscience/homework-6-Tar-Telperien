37639f5ef989ae7cb048c414867930b3c3878d10
Amelia Brown <abrown@Roberts-Mac-mini.local>
2019-10-31 19:55:44 -0500

    Running homework

--- Homework6/Homework6/ViewController.swift
+++ Homework6/Homework6/ViewController.swift
@@ -15,13 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet var selectionButton: UIButton!
     @IBOutlet var continueButton: UIButton!
     
+    var segmentedControllerCorrect: Bool = false
+    var selectionSwitchOn: Bool = false
+    var selectionButtonPressed: Bool = false
+    
     @IBAction func segmentedControllerDidChangeValue(_ sender: UISegmentedControl) {
+        segmentedControllerCorrect = segmentedController.selectedSegmentIndex == 1
+        enableContinueButton()
     }
     
     @IBAction func selectionSwitchDidChangeValue(_ sender: UISwitch) {
+        selectionSwitchOn = selectionSwitch.isOn
+        enableContinueButton()
     }
     
     @IBAction func selectionButtonTapped(_ sender: UIButton) {
+        selectionButton.isSelected.toggle()
+        selectionButtonPressed = selectionButton.isSelected
+        enableContinueButton()
+    }
+    
+    func enableContinueButton() {
+        if selectionSwitch.isOn && selectionButton.isSelected && segmentedController.selectedSegmentIndex == 1 {
+            continueButton.isEnabled = true
+        }
     }
 }
 
